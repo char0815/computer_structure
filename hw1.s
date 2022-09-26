28639757b3c166775f8e807b18ec7ad9d34c492d
char0815 <huangcharlie2@gmail.com>
2022-09-26 16:15:49 +0800

    Update hw1.s

--- hw1.s
+++ hw1.s
@@ -1,48 +1,38 @@
-buildArray:
-        addi    sp,sp,-48
-        sw      ra,44(sp)
-        sw      s0,40(sp)
-        addi    s0,sp,48
-        sw      a0,-36(s0)
-        sw      a1,-40(s0)
-        sw      a2,-44(s0)
-        sw      zero,-20(s0)
-        lw      a5,-40(s0)
-        slli    a5,a5,2
-        mv      a0,a5
-        call    malloc
-        mv      a5,a0
-        sw      a5,-24(s0)
-        sw      zero,-20(s0)
-        j       .L2
-.L3:
-        lw      a5,-20(s0)
-        slli    a5,a5,2
-        lw      a4,-36(s0)
-        add     a5,a4,a5
-        lw      a5,0(a5)
-        slli    a5,a5,2
-        lw      a4,-36(s0)
-        add     a4,a4,a5
-        lw      a5,-20(s0)
-        slli    a5,a5,2
-        lw      a3,-24(s0)
-        add     a5,a3,a5
-        lw      a4,0(a4)
-        sw      a4,0(a5)
-        lw      a5,-20(s0)
-        addi    a5,a5,1
-        sw      a5,-20(s0)
-.L2:
-        lw      a4,-20(s0)
-        lw      a5,-40(s0)
-        blt     a4,a5,.L3
-        lw      a5,-44(s0)
-        lw      a4,-40(s0)
-        sw      a4,0(a5)
-        lw      a5,-24(s0)
-        mv      a0,a5
-        lw      ra,44(sp)
-        lw      s0,40(sp)
-        addi    sp,sp,48
-        jr      ra
+.data
+num: .word 0,2,1,5,3,4
+numSize: .word 6
+.text
+main:
+    la a1,num          #get num's address
+    la a2,numSize      #get numSize's address
+    lw a2,0(a2)        #a2=6
+    addi a2,a2,-1      #because matrix is start at 0,so a2 must minus 1
+    addi a3,zero,0     #set i=0
+    j end.L2
+end.L1:
+    slli t0,a3,2       #calculate num[i] address
+    add t0,t0,a1       #calculate num[i] address
+    lw t1,0(t0)        #load num[i] to t1
+    slli t1,t1,2       #calculate num[numn[i]] address
+    add,t1,t1,a1       #calculate num[numn[i]] address
+    lw t2,0(t1)        #load num[numn[i]] to t2
+    slli t3,a3,2       #calculate p[i] address
+    add t3,a4,t3       #calculate p[i] address
+    sw t2,0(t3)        #let p[i] = num[mum[i]]
+    addi a3,a3,1       #i++
+end.L2:
+    ble a3,a2,end.L1   #if i<a2,jump to L1
+    addi a3,zero,0     #set i=0
+    j print
+print:
+    slli t3,a3,2       #calculate p[i] address
+    add t3,a4,t3       #calculate p[i] address
+    lw a0,0(t3)        #load p[i] to a0
+    li a7, 1           #system call print
+    ecall              #execute system call
+    addi a3,a3,1       #i++
+    ble a3,a2,print    #if i<a2,jump to print
+    j exit
+exit:
+    li a7, 10          # system call exit
+    ecall              #execute system call
